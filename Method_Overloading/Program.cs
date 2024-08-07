dc8c743be0a7a67c0cbd986fa048e29073f2c1e9
farid <farid@LAPTOP-SPTVSQVM>
2024-08-07 23:35:58 +0530

    Add project files.

---
+++ Method_Overloading/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Overloading
+{
+    public class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("after adding 1+2:");
+            int res1 = overloading.Add(1, 2);
+            Console.WriteLine(res1);
+            Console.WriteLine("after adding 1+2+3:");
+            int res2 = overloading.Add(1,2,3);
+            Console.WriteLine(res2);
+        }
+    }
+}
---
+++ Method_Overloading/overloading.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Overloading
+{
+    public class overloading
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Add(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+
+        public static int Add()
+        {
+            return 0;
+        }
+
+    }
+}
